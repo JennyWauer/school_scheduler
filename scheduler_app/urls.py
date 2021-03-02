826468a2202ff74468abda40b6f2e5b84bd33a3a
AishaE <aishaelahi@gmail.com>
2021-03-01 23:56:43 -0500

    updates from Aisha

--- scheduler_app/urls.py
+++ scheduler_app/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
 	path('create_user', views.create_user),
 	path('profile', views.class_profile),
 	path('login', views.user_login),
+	path('all_classes',views.all_classes),
+	path('add_student',views.add_student),
+	path('edit_assign',views.edit_assign),
+	path('student_assign',views.student_assign),
+	path('delete/<int:student_id>',views.delete_student),
 	path('logout',views.logout),
 ]
--- scheduler_app/views.py
+++ scheduler_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import Users, UsersManager,studentsManager, students,subjectManager,subjects,assignmentsManager,assignments,messagesManager,messages
+from .models import User, UserManager,StudentManager, Student,SubjectManager,Subject, AssignmentManager, Assignment, MessageManager, Message
 from django.contrib import messages
 import bcrypt
 # Create your views here.
@@ -15,12 +15,32 @@ def index(request):
 def add_user(request):
 	return render(request, 'login_reg.html')
 
+def all_classes(request):
+	context = {
+		"all_student": Student.objects.all()
+	}
+	return render(request, 'all_classes.html', context)
+
+def edit_assign(request):
+	return render(request, 'edit_assignment.html')
+
+def student_assign(request):
+	context = {
+		"assign": Assignment.objects.all(),
+		"teacher" : User.objects.all(),
+	}
+	return render(request, 'student_page.html')
+
+def class_profile(request):
+	context = {
+		"user": User.objects.get(id=request.session['user_id'])
+	}
+	return render(request, 'profile.html',context)
+
 def logout(request):
 	request.session.flush()
 	return redirect('/')
 
-def class_profile(request):
-	return render(request, 'profile.html')
 
 #<---------POST METHODS------>
 
@@ -29,21 +49,21 @@ def create_user(request):
 	print(" Can I create a user?!")
 	if request.method != "POST":
 		return redirect('/')
-	errors = Users.objects.validate(request.POST)
+	errors = User.objects.validate(request.POST)
 		#if the dictionary received has errors in it, reject the form, and show the error messages
 		# on the template the user was on last
 	if len(errors) > 0:
 		print(errors)
 		for key, value in errors.items():
 			messages.error(request, value)
-			return redirect('/')
+		return redirect('/register')
 	else:
 		user_pw=request.POST['password']
 		# create the hash for the password
 		hash_pw=bcrypt.hashpw(user_pw.encode(), bcrypt.gensalt()).decode()
 		print(hash_pw)
 		# create user object 
-		new_user = Users.objects.create(
+		new_user = User.objects.create(
 			first_name=request.POST['first_name'], 
 			last_name=request.POST['last_name'], 
 			email=request.POST['email'], 
@@ -63,7 +83,7 @@ def user_login(request):
 	print('Is this user_login method working?')
 	if request.method == 'POST':
 		# query to find the user
-		logged_user=Users.objects.filter(email=request.POST['email'])
+		logged_user=User.objects.filter(email=request.POST['email'])
 
 		if len(logged_user) > 0:
 			logged_user = logged_user[0]
@@ -81,3 +101,27 @@ def user_login(request):
 			messages.error(request, "Your email does not exist.")
 			return redirect ('/')
 	return redirect('/profile')
+
+#POST: Create / Add student
+def add_student(request):
+	print('Can I add a student to the db? ')
+	if request.method == "POST":
+		error = Student.objects.validate(request.POST)
+		if error:
+			messages.error(request, error)
+			return redirect ('/all_classes')
+		add_new_student = Student.objects.create(
+			first_name = request.POST['first_name'],
+			last_name = request.POST['last_name'],
+		)
+		user = User.objects.get(id=request.session['user_id'])
+		#add student to user = ie teacher
+		user.user_students.add(add_new_student)
+	return redirect ('/all_classes')
+
+
+def delete_student (request, student_id):
+	print("I want to delete a student!")
+	delete_student=Student.objects.get(id=student_id)
+	delete_student.delete()
+	return redirect ('/all_classes')
